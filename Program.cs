70d2c604086f7a7cec184415128a131380ea97a1
Natalya <can-natasha@yzndex.ru>
2023-02-15 21:15:21 +0500

    задача2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else if (b > a && b > c)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
+else 
+{
+    Console.Write("max = ");
+    Console.WriteLine(c);
+}
